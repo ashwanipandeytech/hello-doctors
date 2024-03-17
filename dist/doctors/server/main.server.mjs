6b8c4fdb27e7ffd7959bd2896215f2d7efb393a9
Ashwani <ashwani.pandey@myqsoft.com>
2024-03-17 20:46:53 +0530

    done

--- dist/doctors/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-V67QUNWR.mjs";import"./chunk-B36DALWP.mjs";import"./chunk-YMLIWOHJ.mjs";import"./chunk-W64HUE6K.mjs";import"./chunk-GAZFTVM6.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
